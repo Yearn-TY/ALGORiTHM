1613d1cb9cd46ee5bf4f51ef505e00794ff14ee9
ALL <44322103+Yearn-TY@users.noreply.github.com>
2022-11-15 14:01:22 +0800

    tree

--- base/src/Main.java
+++ base/src/Main.java
@@ -1,11 +1,36 @@
+import tree.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static tree.Tree.*;
+
 /**
  * @author all
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        List<Node> list = new ArrayList<>();
+        Node head = initNode(new Node(), 3);
+        inorderTraversal(head, list);
+        for (Node node : list) {
+            System.out.print(node.getValue() + " ");
+        }
+        System.out.println();
+        List<Node> list2 = new ArrayList<>();
+        preorderTraversal(head, list2);
+        for (Node node : list2) {
+            System.out.print(node.getValue() + " ");
+        }
+        System.out.println();
+        List<Node> list3 = new ArrayList<>();
+        postorderTraversal(head, list3);
+        for (Node node : list3) {
+            System.out.print(node.getValue() + " ");
+        }
     }
+
 
 
 }
